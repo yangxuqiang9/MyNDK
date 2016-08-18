dc84b3202ffee7ce227af25928913fa864e2573b
yxq <932477876@qq.com>
2016-08-18 15:04:37 +0800

    添加获取网络连接类型的工具类

--- app/src/main/java/com/example/a328789/mysdk/SDK/LocationInfo/Utils.java
+++ app/src/main/java/com/example/a328789/mysdk/SDK/LocationInfo/Utils.java
@@ -3,8 +3,11 @@ package com.example.a328789.mysdk.SDK.LocationInfo;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.telephony.TelephonyManager;
+
+import java.util.ArrayList;
 
 /**
  * Created by 328789 on 2016/8/17.
@@ -13,11 +16,27 @@ public class Utils {
     /**
      * 获取本地联系人
      */
-    public void getPhoneContact(Context context){
-        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        ContentResolver contentResolver = context.getContentResolver();
-        Cursor query = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+    public ArrayList<String> getContactList(Context context){
 
+        ArrayList<String> list= new ArrayList<String>();
 
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String[] columns    = new String[] {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+        Cursor people = context.getContentResolver().query(uri, columns, null, null, null);
+
+        int _nameIndex = people.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+        int _numberIndex = people.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+
+        people.moveToFirst();
+        do {
+            String name   = people.getString(_nameIndex);
+            String number = people.getString(_numberIndex);
+            list.add(name+" :"+number);
+            // Do work...
+        } while (people.moveToNext());
+
+        return list;
     }
 }
