3ad74070ad8cd5014aa16be121eeae7b55b16b71
yxq <932477876@qq.com>
2016-08-20 17:36:23 +0800

    增加了获取联系人的工具类

--- app/src/main/java/com/example/a328789/mysdk/SDK/LocationInfo/Utils.java
+++ app/src/main/java/com/example/a328789/mysdk/SDK/LocationInfo/Utils.java
@@ -1,10 +1,18 @@
 package com.example.a328789.mysdk.SDK.LocationInfo;
 
+import android.app.Activity;
+import android.app.Notification;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -36,6 +44,49 @@ public class Utils {
         } while (people.moveToNext());
 
         return list;
+    }
+    /**
+     * 打开手机相册
+     */
+    public void openPhoto(Activity activity, int requestCode){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        activity.startActivityForResult(intent,requestCode);
+    }
+    /**
+     *
+     */
+    /**
+     * 在onActivityResult中获取图片的uri
+     */
+    public Bitmap getImageUri(Context context,Intent data){
+        Uri photoUri = data.getData();
+        Bitmap bitmap=null;
+        Bitmap bp=null;
+        if(photoUri==null){
+            Bundle extras = data.getExtras();
+            bp= (Bitmap) extras.get("data");
+            bitmap = Bitmap.createBitmap(bp, 0, 0, bp.getWidth(), bp.getHeight(), null, true);
+        }else{
+            Cursor cursor = context.getContentResolver().query(photoUri,
+                    new String[] { MediaStore.Images.ImageColumns.ORIENTATION }, null, null, null);
+            if(cursor==null){
+                return null;
+            }
+            cursor.moveToFirst();
+            int anInt = cursor.getInt(0);
+            cursor.close();
+            Matrix matrix = new Matrix();
+            matrix.postRotate(anInt);
+            try {
+                bp = MediaStore.Images.Media.getBitmap(context.getContentResolver(),photoUri);
+                bitmap = Bitmap.createBitmap(bp, 0, 0, bp.getWidth(), bp.getHeight(), matrix, true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return bitmap;
     }
     /**
      * 获取本地相册照片
