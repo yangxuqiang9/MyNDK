724fc6ba404de18b783eafd0bc71d27aa4c30f2d
yangxuqiang <932477876@qq.com>
2017-01-11 14:46:29 +0800

    添加截屏功能

--- app/src/main/java/com/example/a328789/mysdk/SDK/TOOLS/ScreenUtils.java
+++ app/src/main/java/com/example/a328789/mysdk/SDK/TOOLS/ScreenUtils.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
-import android.graphics.RectF;
 import android.view.View;
 
 /**
